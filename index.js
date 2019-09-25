6638dd290b563fd4d7264b198478d72a0718e3cb
Mursaleen Taqi <leemursa@gmail.com>
2019-09-25 15:14:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+const gifts = ['Lisa','Kaitlin','Jan'];
+function writeCards(gifts){
+  for(let i=0; i<gifts.length; i++)
+  console.log (gifts[i] =`Thank you, ${gifts[i]}, for the wonderful surprise gift!`);
+}
+
+function countdown (number){
+  while(number>=0) {
+    console.log(number)
+
+  number-=1;
+  }
+}
